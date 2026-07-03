449191ded65e58709b49a12dac3286a773e0f885
Corey Gavyn <cgavyn8@outlook.com>
2026-07-03 14:56:00 -0700

    updated SceneRunner

--- vn/engine/SceneRunner.js
+++ vn/engine/SceneRunner.js
@@ -14,8 +14,56 @@ export class SceneRunner {
           }
           break;
 
+        case "textHide":
+          if (typeof this.context.onTextHide === "function") {
+            await this.context.onTextHide();
+          }
+          break;
+
         case "wait":
           await sleep(step.ms ?? 0);
+          break;
+
+        case "render":
+          if (typeof this.context.onRender === "function") {
+            await this.context.onRender(this.context);
+          }
+          break;
+
+        case "revealCard":
+          if (typeof this.context.onRevealCard === "function") {
+            await this.context.onRevealCard(step.index, step.sound);
+          }
+          break;
+
+        case "revealAll":
+          if (typeof this.context.onRevealAll === "function") {
+            await this.context.onRevealAll();
+          }
+          break;
+
+        case "bars":
+          if (typeof this.context.onBars === "function") {
+            await this.context.onBars();
+          }
+          break;
+
+        case "theme":
+          if (typeof this.context.onTheme === "function") {
+            await this.context.onTheme(step.color);
+          }
+          break;
+
+        case "finalText":
+          if (typeof this.context.onFinalText === "function") {
+            await this.context.onFinalText(step.value);
+          }
+          break;
+
+        case "hideOverlay":
+          if (typeof this.context.onHideOverlay === "function") {
+            await this.context.onHideOverlay();
+          }
           break;
 
         case "action":
