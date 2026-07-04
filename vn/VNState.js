7d1cb00ecc0d1df52f9aa73f462351d76b9c0b18
Corey Gavyn <cgavyn8@outlook.com>
2026-07-03 17:25:25 -0700

    changed vnstate again

--- vn/VNState.js
+++ vn/VNState.js
@@ -1,105 +1,69 @@
 export const VNState = {
-  // Quiz data
   questions: [],
   personalities: [],
 
-  // Current position
   currentQuestion: 0,
-
-  // One answer per question
   answers: [],
+  results: null,
 
-  // Calculated results
-  results: [],
-
-  /**
-   * Initialize a new quiz session
-   */
   init({ questions, personalities }) {
-    this.questions = [...questions];
-    this.personalities = [...personalities];
+    this.questions = questions || [];
+    this.personalities = personalities || [];
 
-    this.currentQuestion = 0;
-    this.answers = new Array(this.questions.length).fill(null);
-    this.results = [];
+    this.reset();
   },
 
-  /**
-   * Current question object
-   */
+  reset() {
+    this.currentQuestion = 0;
+    this.answers = [];
+    this.results = null;
+  },
+
   getCurrentQuestion() {
     return this.questions[this.currentQuestion] ?? null;
   },
 
-  /**
-   * Total number of questions
-   */
   getQuestionCount() {
     return this.questions.length;
   },
 
-  /**
-   * Current question index
-   */
   getCurrentIndex() {
     return this.currentQuestion;
-  },
-
-  /**
-   * Navigation
-   */
-  hasPrevious() {
-    return this.currentQuestion > 0;
   },
 
   hasNext() {
     return this.currentQuestion < this.questions.length - 1;
   },
 
+  hasPrevious() {
+    return this.currentQuestion > 0;
+  },
+
   nextQuestion() {
-    if (this.hasNext()) {
-      this.currentQuestion++;
-    }
+    if (this.hasNext()) this.currentQuestion++;
   },
 
   previousQuestion() {
-    if (this.hasPrevious()) {
-      this.currentQuestion--;
-    }
+    if (this.hasPrevious()) this.currentQuestion--;
   },
 
-  /**
-   * Answers
-   */
   setAnswer(index, value) {
     this.answers[index] = value;
   },
 
   getAnswer(index) {
-    return this.answers[index] ?? null;
+    return this.answers[index];
   },
 
   getAnswers() {
     return [...this.answers];
   },
 
-  /**
-   * Results
-   */
   setResults(results) {
-    this.results = [...results];
+    this.results = results;
   },
 
   getResults() {
-    return [...this.results];
-  },
-
-  /**
-   * Reset current playthrough
-   */
-  reset() {
-    this.currentQuestion = 0;
-    this.answers = new Array(this.questions.length).fill(null);
-    this.results = [];
+    return this.results;
   },
 };
