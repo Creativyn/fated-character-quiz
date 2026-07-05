50ca32dec9cdb1bb19fe05db1fc0a9ef1be70f37
Corey Gavyn <cgavyn8@outlook.com>
2026-07-05 09:46:22 -0700

    correct changes made to script.js for the toggle.

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-localStorage.removeItem("fatedQuiz.skipCinematic");
-location.reload();
+import { QUESTIONS } from "./data/questions.js";
+import { PERSONALITIES } from "./config/personalities.js";
+import { VNEngine } from "./vn/VNEngine.js";
+import {
+  getSkipCinematicPreference,
+  setSkipCinematicPreference,
+} from "./utils/preferenceManager.js";
+
+function bindQuizPreferences() {
+  const skipToggle = document.getElementById("skip-cinematic");
+
+  if (!skipToggle) return;
+
+  const savedSkip = getSkipCinematicPreference(false);
+
+  skipToggle.checked = savedSkip;
+
+  skipToggle.addEventListener("change", () => {
+    setSkipCinematicPreference(skipToggle.checked);
+  });
+}
+
+async function bootApp() {
+  bindQuizPreferences();
+
+  await VNEngine.start({
+    questions: QUESTIONS,
+    personalities: PERSONALITIES,
+  });
+
+  console.log("VN Engine running");
+}
+
+bootApp();
